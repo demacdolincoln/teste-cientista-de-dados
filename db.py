e1fdf6369046a9458dabf07dd3a7b3df31c113bf
Lincoln de Macêdo <lincoln000@protonmail.com>
2020-10-03 02:12:39 -0300

    export json to sql: ok

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from peewee import *
+from dateutil.parser import parse as dateparse
 
-db = SqliteDatabase("./recept.db")
+db = SqliteDatabase("/tmp/recept.db")
 
 
 class BaseModel(Model):
@@ -20,21 +21,76 @@ class Recept(BaseModel):
     title = CharField(null=True)
     date = DateTimeField(null=True)
 
+    def create(
+        self=None,
+        calories=None,
+        fat=None,
+        protein=None,
+        rating=None,
+        sodium=None,
+        desc=None,
+        directions=None,
+        ingredients=None,
+        title=None,
+        date=None,
+    ):
+        self.calories = calories
+        self.fat = fat
+        self.protein = protein
+        self.rating = rating
+        self.sodium = sodium
+        self.desc = desc
+        if directions is not None:
+            self.directions = "".join(map(lambda x: x+"\n", directions))
+        if ingredients is not None:
+            self.ingredients = "".join(map(lambda x: x+"\n", ingredients))
+        self.title = title
+        if date is not None:
+            self.date = dateparse(date)
+
 
 class Categorie(BaseModel):
-    name = CharField()
+    name = CharField(unique=True)
 
 
 class CategorieRecept(BaseModel):
-    categorie = ForeignKeyField(Categorie)
-    recept = ForeignKeyField(Recept)
+    categorie = ForeignKeyField(Categorie, backref="recepts")
+    recept = ForeignKeyField(Recept, backref="categories")
 
     class Meta:
-        primary_key = CompositeKey('categorie', 'recept')
+        primary_key = CompositeKey("categorie", "recept")
 
 
 db.connect()
 db.create_tables([Categorie, Recept, CategorieRecept])
 
 if __name__ == "__main__":
-    pass
+    import json
+    from sys import argv
+    from tqdm import tqdm
+
+    def init_categories(names):
+        for name in names:
+            entity, _ = Categorie.get_or_create(name=name.lower())
+            yield entity
+
+    with open(argv[1]) as json_file:
+        js = json.load(json_file)
+
+        for data in tqdm(js):
+            categories = data.pop(
+                "categories") if "categories" in data.keys() else None
+
+            recept = Recept()
+            recept.create(**data)
+            recept.save()
+
+            if categories is not None:
+                entity_categories = init_categories(
+                    categories
+                )
+                for categorie in entity_categories:
+                    categorie_recept = CategorieRecept.create(
+                        categorie=categorie,
+                        recept=recept
+                    )
